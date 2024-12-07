5f5112ed522333b8369feab50870637ce7fd9167
Frank <khanhvo.frank@gmail.com>
2024-12-07 13:31:18 +0700

    created ecom app, better Add to Cart and remove function, added SupperCoins

--- src/Components/ProductList.jsx
+++ src/Components/ProductList.jsx
@@ -1,7 +1,12 @@
-import React from 'react';
-import './ProductList.css'; 
+import React, { useState, useEffect } from 'react';
+import './ProductList.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { addItemToCart } from './CartSlice';
 
 const ProductList = () => {
+  const dispatch = useDispatch();
+  const cartItems = useSelector(state => state.cart.cartItems); // Lấy các sản phẩm trong giỏ hàng từ Redux store
+  const [disabledProducts, setDisabledProducts] = useState([]);
 
   const products = [
     { id: 1, name: 'Product A', price: 60 },
@@ -9,11 +14,30 @@ const ProductList = () => {
     { id: 3, name: 'Product C', price: 30 },
   ];
 
+  useEffect(() => {
+    const cartProductIds = cartItems.map(item => item.id); // khi item trong cart được update, lấy thông tin đó, lấy id và đưa vào disabled list
+    setDisabledProducts(cartProductIds);
+  }, [cartItems]); 
+
+  const handleAddToCart = (product) => {
+    dispatch(addItemToCart(product)); // Thêm sản phẩm vào giỏ hàng
+  };
+
   return (
     <div className="product-list">
       <h2 className="product-list-title">Products</h2>
       <ul className="product-list-items">
-     
+        {products.map(product => (
+          <li key={product.id} className="product-list-item">
+            <span>{product.name} - ${product.price}</span>
+            <button
+              className={`add-to-cart-btn ${disabledProducts.includes(product.id) ? "disabled" : ""}`}
+              onClick={() => handleAddToCart(product)}
+              disabled={disabledProducts.includes(product.id)}>
+              Add to Cart
+            </button>
+          </li>
+        ))}
       </ul>
     </div>
   );
--- src/Components/ShoppingCart.jsx
+++ src/Components/ShoppingCart.jsx
@@ -1,16 +1,63 @@
 import React from 'react';
-import './ShoppingCart.css'; 
+import './ShoppingCart.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { removeItemFromCart, clearCart, increaseItemQuantity, decreaseItemQuantity } from './CartSlice';
 
 const ShoppingCart = () => {
+
+  const dispatch = useDispatch();
+  const cartItems = useSelector(state => state.cart.cartItems);
+
+  const totalAmount = cartItems.reduce((total, item) => total + item.price * item.quantity, 0);
+  /*
+  reduce() dùng để "thu gọn" thành 1 giá trị duy nhất
+  array.reduce((accumulator, currentValue) => {}, initialValue);
+    accumulator: Biến lưu giá trị tích lũy qua từng vòng lặp.
+    currentValue: item đang được xử lý ở vòng lặp hiện tại
+    0 = giá trị ban đầu
+    Lần 1: item = { id: 1, price: 100, quantity: 2 }
+    => total = 0 + 100 * 2 = 200.
+    Lần 2: item = { id: 2, price: 50, quantity: 3 }
+    => total = 200 + 50 * 3 = 350.
+  */
+
+  const handleRemoveItem = itemId => {
+    dispatch(removeItemFromCart(itemId));
+  }
+  
+  const handleClearCart = () => {
+    dispatch(clearCart());
+  }
+
+  const handleIncreaseQuantity = itemId => {
+    dispatch(increaseItemQuantity(itemId));
+  }
+
+  const handleDecreaseQuantity = itemId => {
+    dispatch(decreaseItemQuantity(itemId));
+  }
 
   return (
     <>
     <div className="shopping-cart">
       <h2 className="shopping-cart-title">Shopping Cart</h2>
       <ul className="cart-items">
-       
+        {cartItems.map((item) => (
+          <li key={item.id} className="cart-item">
+            <span>{item.name} - ${item.price}</span>
+            <div className="quantity-controls">
+              <button className="quantity-control-btn" onClick={() => handleDecreaseQuantity(item.id)}>-</button>
+              <span> &nbsp; {item.quantity} &nbsp; </span>
+              <button className="quantity-control-btn" onClick={() => handleIncreaseQuantity(item.id)}>+</button>
+            </div>
+            <button className="remove-item-btn" onClick={() => handleRemoveItem(item.id)}>Remove</button>
+          </li>
+        ))}
       </ul>
-      <button className="clear-cart-btn">Clear Cart</button>
+      <button className="clear-cart-btn" onClick={handleClearCart}>Clear Cart</button>
+      <br></br>
+      <br></br>
+      <h4>{totalAmount ? <div>The total amount is: ${totalAmount}</div> : ''}</h4>
     </div>
   
     </>
